a88e755fbf0fc9b1f92cde242e39e6d025075a4c
TresLos <asistare7@gmail.com>
2017-11-17 09:54:13 +0100

    Per quan reinstal·la el vim dins d'arch.

---
+++ actualització/vim.sh
@@ -0,0 +1,5 @@
+echo "set spell"		>> /usr/share/vim/vim80/defaults.vim
+echo "set spelllang=ca"		>> /usr/share/vim/vim80/defaults.vim
+echo "set colorcolumn=80"	>> /usr/share/vim/vim80/defaults.vim
+echo "set mouse="		>> /usr/share/vim/vim80/defaults.vim
+echo "colorscheme ron"		>> /usr/share/vim/vim80/defaults.vim
